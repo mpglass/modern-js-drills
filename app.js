501b1c2ad8cae6df4b86278ed2562f54644f25b0
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-05-03 20:50:09 -0500

    arrow functions done

---
+++ app.js
@@ -0,0 +1,23 @@
+let favMovie = (name, movie) => console.log(`My name is ${name} and my favorite movie is ${movie}.`)
+
+favMovie('Michelle', 'The Greatest Showman');
+
+let myName = 'Michelle Glass';
+let name = myName.split(' ');
+let getFirstName = (name) => {
+    return `${name[0]}`;
+};
+let result1 = getFirstName(name);
+let getFirstNameConcise = (name) => `${name[0]}`;
+let result2 = getFirstNameConcise(name);
+console.log(result1)
+console.log(result2)
+
+function getStats(a,b) {
+    let exponent = a ^ b;
+    let product = a * b; 
+    return { exponent, product };
+}
+let stats = getStats(2, 6);
+console.log(stats.exponent);
+console.log(stats.product);
